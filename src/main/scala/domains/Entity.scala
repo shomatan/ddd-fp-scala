fe4a34a15850a193f8d6282a23d56183c2e99a3e
Shoma Nishitateno <shoma416@gmail.com>
2019-06-20 18:17:13 +0900

    update: don't use equals

--- src/main/scala/domains/Entity.scala
+++ src/main/scala/domains/Entity.scala
@@ -4,13 +4,7 @@ trait Entity[T] {
 
   def id: Id[T]
 
-  override def equals(obj: Any): Boolean =
-    obj match {
-      case that: Entity[_] => this.getClass == that.getClass && this.id == that.id
-      case _ => false
-    }
-
-  override def hashCode(): Int = 31 + id.hashCode
+  def sameIdentity(b: Entity[T]): Boolean = this.id == b.id
 
 }
 
